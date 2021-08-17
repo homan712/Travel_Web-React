29d117bd24de1ee14446fce26cbaef66abce4d74
homan712 <68459928+homan712@users.noreply.github.com>
2021-08-17 10:05:57 -0400

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  return <h1 className='sign-up'>Sign Up</h1>;
 }
